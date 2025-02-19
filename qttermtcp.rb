5f5cd25f43e78c12afdf669c2b6018a2f44e6a94
Stuart Henderson <stuart@craggiemore.com>
2025-02-19 16:34:00 +0000

    Clean up QtTermTCP formula by removing commented-out code and unnecessary documentation

--- qttermtcp.rb
+++ qttermtcp.rb
@@ -1,7 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Qttermtcp < Formula
   desc "QtTermTCP - TCP KISS Terminal for Amateur Radio Packet"
   homepage "https://github.com/g8bpq/QtTermTCP"
@@ -13,8 +9,9 @@ class Qttermtcp < Formula
 
   depends_on "qt"
 
+
   def install
-    system "qmake", "QMAKE_CXXFLAGS+=-I#{Formula[\"qt\"].opt_include}", "QMAKE_LFLAGS+=-F#{Formula[\"qt\"].opt_lib}"
+        system "qmake", "QMAKE_CXXFLAGS+=-I#{Formula[\"qt\"].opt_include}", "QMAKE_LFLAGS+=-F#{Formula[\"qt\"].opt_lib}"
     system "make", "LIBS+=-lQtMultimedia"
     system "make"
 
@@ -28,7 +25,7 @@ class Qttermtcp < Formula
 
     mkdir_p apps_dir
     mv app_path, apps_dir
-end
+  end
 
   def caveats
     <<~EOS
@@ -42,40 +39,3 @@ end
     assert_predicate "/Applications/QtTermTCP.app", :exist?
   end
 end
-
-
-
-# class Qttermtcp < Formula
-#   desc ""
-#   homepage ""
-#   license ""
-#   head "https://github.com/g8bpq/QtTermTCP.git"
-
-#   # depends_on "cmake" => :build
-
-#   # Additional dependency
-#   # resource "" do
-#   #   url ""
-#   #   sha256 ""
-#   # end
-
-#   def install
-#     # Remove unrecognized options if they cause configure to fail
-#     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-#     system "./configure", "--disable-silent-rules", *std_configure_args
-#     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-#   end
-
-#   test do
-#     # `test do` will create, run in and delete a temporary directory.
-#     #
-#     # This test will fail and we won't accept that! For Homebrew/homebrew-core
-#     # this will need to be a test that verifies the functionality of the
-#     # software. Run the test with `brew test QtTermTCP`. Options passed
-#     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-#     #
-#     # The installed folder is not in the path, so use the entire path to any
-#     # executables being tested: `system bin/"program", "do", "something"`.
-#     system "false"
-#   end
-# end
