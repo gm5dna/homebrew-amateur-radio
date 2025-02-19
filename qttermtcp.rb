fbbe3e4b3c320fe7929dde8897bf938d4fa88120
Stuart Henderson <stuart@craggiemore.com>
2025-02-19 19:22:53 +0000

    Refactor QtTermTCP formula to simplify installation process and update caveats for macOS app linking

--- qttermtcp.rb
+++ qttermtcp.rb
@@ -6,63 +6,28 @@ class Qttermtcp < Formula
   sha256 "c01e5ea266833d47469c7aa0d9f7ac582767de771960aaed57d07fd915de6237"
   license "GPL-3.0-or-later"
 
-  depends_on "qt5" => :build
+  depends_on "qt5"
 
   def install
     system "qmake", "QtTermTCP.pro"
     system "make"
     prefix.install "QtTermTCP.app"
-    bin.mkpath
-    ln_sf prefix/"QtTermTCP.app", "/Applications/QtTermTCP.app"
   end
 
   def caveats
     <<~EOS
-      The application has been installed and symlinked to /Applications:
-        /Applications/QtTermTCP.app
-
-      If you encounter issues, you may need to remove the symlink manually:
-        rm /Applications/QtTermTCP.app
+      The application has been installed as a macOS app bundle in:
+        #{opt_prefix}/QtTermTCP.app
+      
+      You can link it to /Applications with:
+        ln -s #{opt_prefix}/QtTermTCP.app /Applications/QtTermTCP.app
     EOS
   end
 
   test do
-    assert_predicate prefix/"QtTermTCP.app", :exist?, "QtTermTCP.app was not installed"
-    assert_predicate Pathname.new("/Applications/QtTermTCP.app"), :symlink?, "Symlink to /Applications was not created"
+    assert_predicate prefix/"QtTermTCP.app", :exist?, "QtTermTCP.app bundle was not installed"
   end
 end
-
-
-# class Qttermtcp < Formula
-#   desc "QtTermTCP terminal application"
-#   homepage "https://github.com/g8bpq/QtTermTCP"
-#   url "https://github.com/g8bpq/QtTermTCP/archive/refs/tags/0.79.zip"
-#   version "0.0.0.79"
-#   sha256 "c01e5ea266833d47469c7aa0d9f7ac582767de771960aaed57d07fd915de6237"
-#   license "GPL-3.0-or-later"
-
-#   depends_on "qt5"
-
-#   def install
-#     system "qmake", "QtTermTCP.pro"
-#     system "make"
-#     prefix.install "QtTermTCP.app"
-#   end
-
-#   def caveats
-#     <<~EOS
-#       The application has been installed as a macOS app bundle in:
-#         #{opt_prefix}/QtTermTCP.app
-      
-#       You can link it to /Applications with:
-#         ln -s #{opt_prefix}/QtTermTCP.app /Applications/QtTermTCP.app
-#     EOS
-#   end
-
-#   test do
-#     assert_predicate prefix/"QtTermTCP.app", :exist?, "QtTermTCP.app bundle was not installed"
-#   end
-# end
 
 #  ---
 
