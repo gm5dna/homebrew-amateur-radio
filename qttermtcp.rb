a2cac5ebd46a9a1923eaa8ae1cfb4b0e59dc8fc3
Stuart Henderson <stuart@craggiemore.com>
2025-02-19 19:07:44 +0000

    Add post-install step to create symlink in /Applications for easier access

--- qttermtcp.rb
+++ qttermtcp.rb
@@ -14,13 +14,26 @@ class Qttermtcp < Formula
     prefix.install "QtTermTCP.app"
   end
 
+  def post_install
+    applications_dir = "/Applications"
+    app_path = "#{opt_prefix}/QtTermTCP.app"
+    link_path = "#{applications_dir}/QtTermTCP.app"
+    
+    if File.exist?(link_path)
+      ohai "Removing existing symlink: #{link_path}"
+      File.delete(link_path)
+    end
+    
+    ohai "Creating symlink: #{link_path} -> #{app_path}"
+    system "ln", "-s", app_path, link_path
+  end
+
   def caveats
     <<~EOS
       The application has been installed as a macOS app bundle in:
         #{opt_prefix}/QtTermTCP.app
       
-      You can link it to /Applications with:
-        ln -s #{opt_prefix}/QtTermTCP.app /Applications/QtTermTCP.app
+      A symlink has been created in /Applications for easier access.
     EOS
   end
 
@@ -30,6 +43,38 @@ class Qttermtcp < Formula
 end
 
 
+# class Qttermtcp < Formula
+#   desc "QtTermTCP terminal application"
+#   homepage "https://github.com/g8bpq/QtTermTCP"
+#   url "https://github.com/g8bpq/QtTermTCP/archive/refs/tags/0.79.zip"
+#   version "0.0.0.79"
+#   sha256 "c01e5ea266833d47469c7aa0d9f7ac582767de771960aaed57d07fd915de6237"
+#   license "GPL-3.0-or-later"
+
+#   depends_on "qt5"
+
+#   def install
+#     system "qmake", "QtTermTCP.pro"
+#     system "make"
+#     prefix.install "QtTermTCP.app"
+#   end
+
+#   def caveats
+#     <<~EOS
+#       The application has been installed as a macOS app bundle in:
+#         #{opt_prefix}/QtTermTCP.app
+      
+#       You can link it to /Applications with:
+#         ln -s #{opt_prefix}/QtTermTCP.app /Applications/QtTermTCP.app
+#     EOS
+#   end
+
+#   test do
+#     assert_predicate prefix/"QtTermTCP.app", :exist?, "QtTermTCP.app bundle was not installed"
+#   end
+# end
+
+#  ---
 
 # class Qttermtcp < Formula
 #   desc "QtTermTCP terminal application"
