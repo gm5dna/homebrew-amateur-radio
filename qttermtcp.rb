62575ca4ce9effff0f216ba1cc0179dbde1c996a
Stuart Henderson <stuart@craggiemore.com>
2025-02-19 16:27:12 +0000

    Add URL, version, and SHA256 for QtTermTCP formula

--- qttermtcp.rb
+++ qttermtcp.rb
@@ -5,6 +5,10 @@
 class Qttermtcp < Formula
   desc "QtTermTCP - TCP KISS Terminal for Amateur Radio Packet"
   homepage "https://github.com/g8bpq/QtTermTCP"
+  url "https://github.com/g8bpq/QtTermTCP/archive/refs/heads/master.zip"
+  version "2025.02.19" # Use a date-based version since there are no releases
+  sha256 "21b39b32e44989579f5c07d8a1432adc57e751f03a916edf3747de9c58fdb346"
+
   head "https://github.com/g8bpq/QtTermTCP.git", branch: "master"
 
   depends_on "qt"
