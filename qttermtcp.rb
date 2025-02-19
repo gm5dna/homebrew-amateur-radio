c1ab02c771c9456fc12152bf9d1de29dab95b472
Stuart Henderson <stuart@craggiemore.com>
2025-02-19 19:16:41 +0000

    Convert QtTermTCP formula to Cask and simplify installation process

--- qttermtcp.rb
+++ qttermtcp.rb
@@ -1,52 +1,22 @@
-class Qttermtcp < Formula
-  desc "QtTermTCP terminal application"
-  homepage "https://github.com/g8bpq/QtTermTCP"
-  url "https://github.com/g8bpq/QtTermTCP/archive/refs/tags/0.79.zip"
+cask "qttermtcp" do
   version "0.0.0.79"
   sha256 "c01e5ea266833d47469c7aa0d9f7ac582767de771960aaed57d07fd915de6237"
-  license "GPL-3.0-or-later"
 
-  depends_on "qt5"
+  url "https://github.com/g8bpq/QtTermTCP/archive/refs/tags/0.79.zip"
+  name "QtTermTCP"
+  desc "QtTermTCP terminal application"
+  homepage "https://github.com/g8bpq/QtTermTCP"
 
-  def install
-    system "qmake", "QtTermTCP.pro"
-    system "make"
-    prefix.install "QtTermTCP.app"
-  end
+  depends_on formula: "qt5"
 
-  def post_install
-    applications_dir = "#{ENV["HOME"]}/Applications"
-    app_path = "#{opt_prefix}/QtTermTCP.app"
-    link_path = "#{applications_dir}/QtTermTCP.app"
-    
-    mkdir_p applications_dir unless Dir.exist?(applications_dir)
-    
-    if File.exist?(link_path)
-      ohai "Removing existing symlink: #{link_path}"
-      File.delete(link_path)
-    end
-    
-    ohai "Creating symlink: #{link_path} -> #{app_path}"
-    system "ln", "-s", app_path, link_path
-  end
+  app "QtTermTCP.app"
 
-  def caveats
-    <<~EOS
-      The application has been installed as a macOS app bundle in:
-        #{opt_prefix}/QtTermTCP.app
-      
-      A symlink has been created in ~/Applications for easier access.
-      If you prefer a symlink in /Applications, run:
-        sudo ln -s #{opt_prefix}/QtTermTCP.app /Applications/QtTermTCP.app
-    EOS
-  end
-
-  test do
-    assert_predicate prefix/"QtTermTCP.app", :exist?, "QtTermTCP.app bundle was not installed"
-  end
+  zap trash: [
+    "~/Library/Preferences/com.g8bpq.QtTermTCP.plist",
+    "~/Library/Application Support/QtTermTCP",
+    "~/Library/Saved Application State/com.g8bpq.QtTermTCP.savedState"
+  ]
 end
-
-
 
 # class Qttermtcp < Formula
 #   desc "QtTermTCP terminal application"
