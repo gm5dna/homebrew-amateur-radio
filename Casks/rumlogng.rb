efb45f827a6245b42a908ca4613cc91cddc7731e
Stuart Henderson <stuart@craggiemore.com>
2025-02-20 12:32:30 +0000

    Add rumlogng cask for HAM radio logging software and update README.md

---
+++ Casks/rumlogng.rb
@@ -0,0 +1,11 @@
+cask "rumlogng" do
+  version "5.19.1"
+  sha256 :no_check
+
+  url "http://dl2rum.de/RUMlogNG/RUMlogNG.zip"
+  name "RUMlogNG"
+  desc "HAM radio logging software by DL2RUM"
+  homepage "http://dl2rum.de/"
+
+  app "RUMlogNG.app"
+end
