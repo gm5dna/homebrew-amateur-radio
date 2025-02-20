04a0f8d332853ca75cda248e4bd9017c35aef5a1
Stuart Henderson <stuart@craggiemore.com>
2025-02-20 10:23:48 +0000

    Add verification to FreeDV cask URL for improved security

--- Casks/freedv.rb
+++ Casks/freedv.rb
@@ -2,7 +2,8 @@ cask "freedv" do
   version "2.0.0-20250130"
   sha256 "da63320c1b65342981c58b36118b752845afbc4dacec0197125650686578884c"
 
-  url "https://github.com/drowe67/freedv-gui/releases/download/v#{version}/FreeDV.dmg"
+  url "https://github.com/drowe67/freedv-gui/releases/download/v#{version}/FreeDV.dmg",
+      verified: "github.com/drowe67/freedv-gui/"
   name "FreeDV"
   desc "Open-source digital voice for HF radio"
   homepage "https://freedv.org/"
