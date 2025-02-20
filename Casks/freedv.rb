ce2b7b278f7ab16ce85bbfa0a86f144fcb2f5599
Stuart Henderson <stuart@craggiemore.com>
2025-02-20 03:38:28 +0000

    Update FreeDV Cask: change name, update URL, and modify description

--- Casks/freedv.rb
+++ Casks/freedv.rb
@@ -1,18 +1,17 @@
 cask "freedv" do
     version "2.0.0-20250130"
-    sha256 "da63320c1b65342981c58b36118b752845afbc4dacec0197125650686578884c"
-    
-    url "https://github.com/drowe67/freedv-gui/releases/download/v#{version}/FreeDV-macOS-#{version}.dmg"
-    name "FreeDV GUI"
-    desc "Digital voice for HF radio"
-    homepage "https://github.com/drowe67/freedv-gui"
+    sha256 da63320c1b65342981c58b36118b752845afbc4dacec0197125650686578884c
+  
+    url "https://github.com/drowe67/freedv-gui/releases/download/v#{version}/FreeDV.dmg"
+    name "FreeDV"
+    desc "Open-source digital voice for HF radio"
+    homepage "https://freedv.org/"
   
     app "FreeDV.app"
   
     zap trash: [
       "~/Library/Application Support/FreeDV",
-      "~/Library/Preferences/org.freedv.plist",
-      "~/Library/Saved Application State/org.freedv.savedState"
+      "~/Library/Preferences/org.freedv.FreeDV.plist"
     ]
   end
   
