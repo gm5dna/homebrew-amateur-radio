ba1836f210f0c7fe929408d866e5497d9bfde84a
Stuart Henderson <stuart@craggiemore.com>
2025-02-20 03:35:52 +0000

    Add Cask for FreeDV GUI with version, description, and installation details

---
+++ Casks/freedv.rb
@@ -0,0 +1,18 @@
+cask "freedv-gui" do
+    version "2.0.0-20250130"
+    sha256 "da63320c1b65342981c58b36118b752845afbc4dacec0197125650686578884c"
+    
+    url "https://github.com/drowe67/freedv-gui/releases/download/v#{version}/FreeDV-macOS-#{version}.dmg"
+    name "FreeDV GUI"
+    desc "Digital voice for HF radio"
+    homepage "https://github.com/drowe67/freedv-gui"
+  
+    app "FreeDV.app"
+  
+    zap trash: [
+      "~/Library/Application Support/FreeDV",
+      "~/Library/Preferences/org.freedv.plist",
+      "~/Library/Saved Application State/org.freedv.savedState"
+    ]
+  end
+  
