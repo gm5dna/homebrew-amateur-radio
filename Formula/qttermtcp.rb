0759ce8aa625d88727a389b0cfdcb32a047d9922
Stuart Henderson <stuart@craggiemore.com>
2025-02-20 10:18:29 +0000

    Update Qttermtcp formula description and dependencies

--- Formula/qttermtcp.rb
+++ Formula/qttermtcp.rb
@@ -1,12 +1,12 @@
 class Qttermtcp < Formula
-  desc "QtTermTCP terminal application"
+  desc "Terminal application for packet radio"
   homepage "https://github.com/g8bpq/QtTermTCP"
-  url "https://github.com/g8bpq/QtTermTCP/archive/refs/tags/0.79.zip"
+  url "https://github.com/g8bpq/QtTermTCP/archive/refs/tags/0.79.tar.gz"
   version "0.0.0.79"
-  sha256 "c01e5ea266833d47469c7aa0d9f7ac582767de771960aaed57d07fd915de6237"
+  sha256 "150752aa261cb4049019cf5f92e06650806c703f00f5bea4f47c9ca834d9c2eb"
   license "GPL-3.0-or-later"
 
-  depends_on "qt5"
+  depends_on "qt@5"
 
   def install
     system "qmake", "QtTermTCP.pro"
@@ -18,13 +18,13 @@ class Qttermtcp < Formula
     <<~EOS
       The application has been installed as a macOS app bundle in:
         #{opt_prefix}/QtTermTCP.app
-      
+
       You can link it to /Applications with:
         ln -s #{opt_prefix}/QtTermTCP.app /Applications/QtTermTCP.app
     EOS
   end
 
   test do
-    assert_predicate prefix/"QtTermTCP.app", :exist?, "QtTermTCP.app bundle was not installed"
+    assert_path_exists prefix/"QtTermTCP.app", :exist?, "QtTermTCP.app bundle was not installed"
   end
 end
