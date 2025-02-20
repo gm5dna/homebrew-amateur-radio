8b268ecff9e7c0bd828996d258c23d7a276c6297
Stuart Henderson <stuart@craggiemore.com>
2025-02-20 03:56:10 +0000

    Add Cask for JS8Call version 2.2.0 with installation details

---
+++ Casks/js8call.rb
@@ -0,0 +1,16 @@
+cask "js8call" do
+    version "2.2.0"
+    sha256 "34fe1b68757bd9ddd3905b443b6d23776b18c5a803944d49a2e3ebf3327c9b9b"
+  
+    url "http://files.js8call.com/2.2.0/js8call-2.2.0-Darwin.dmg"
+    name "JS8Call"
+    homepage "https://js8call.com/"
+  
+    app "JS8Call.app"
+  
+    # zap trash: [
+    #   "~/Library/Application Support/JS8Call",
+    #   "~/Library/Preferences/com.js8call.plist",
+    # ]
+  end
+  
