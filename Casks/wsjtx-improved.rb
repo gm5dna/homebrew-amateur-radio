d366739234c45574aeb4f55d7dd756c4fbfe7b10
Stuart Henderson <stuart@craggiemore.com>
2025-02-20 09:43:32 +0000

    Refactor wsjtx-improved cask for improved readability and consistency

--- Casks/wsjtx-improved.rb
+++ Casks/wsjtx-improved.rb
@@ -1,34 +1,29 @@
 cask "wsjtx-improved" do
-    version "2.7.1,250106-RC8"
+  version "2.7.1,250106-RC8"
 
-    on_arm do
-      sha256 :no_check
-      url "https://sourceforge.net/projects/wsjt-x-improved/files/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}-devel_improved_PLUS_#{version.csv.second}_M1.zip/download",
-          verified: "sourceforge.net/projects/wsjt-x-improved/files/"
-    end
-  
-    on_intel do
-      sha256 :no_check
-      url "https://sourceforge.net/projects/wsjt-x-improved/files/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}-devel_improved_PLUS_#{version.csv.second}_Darwin.zip/download"
-          verified: "sourceforge.net/projects/wsjt-x-improved/files/"
-    end
-  
-    name "WSJT-X Improved"
-    desc "Improved version of WSJT-X"
-    homepage "https://sourceforge.net/projects/wsjt-x-improved/"
-  
-    depends_on macos: ">= :monterey"
-  
-    app "wsjtx.app"
-  
-    postflight do
-      system_command "/bin/mv", args: ["#{appdir}/wsjtx.app", "#{appdir}/wsjtx-improved.app"]
-    end
-
-    zap trash: [
-      "~/Library/Application Support/WSJT-X",
-      "~/Library/Preferences/wsjtx.plist",
-      "~/Library/Saved Application State/wsjtx.savedState"
-    ]
+  on_arm do
+    sha256 :no_check
+    url "https://sourceforge.net/projects/wsjt-x-improved/files/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}-devel_improved_PLUS_#{version.csv.second}_M1.zip/download",
+        verified: "sourceforge.net/projects/wsjt-x-improved/files/"
   end
-  
+
+  on_intel do
+    sha256 :no_check
+    url "https://sourceforge.net/projects/wsjt-x-improved/files/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}-devel_improved_PLUS_#{version.csv.second}_Darwin.zip/download",
+        verified: "sourceforge.net/projects/wsjt-x-improved/files/"
+  end
+
+  name "WSJT-X Improved"
+  desc "Improved version of WSJT-X"
+  homepage "https://sourceforge.net/projects/wsjt-x-improved/"
+
+  depends_on macos: ">= :monterey"
+
+  app "wsjtx.app", target: "wsjtx-improved.app"
+
+  zap trash: [
+    "~/Library/Application Support/WSJT-X",
+    "~/Library/Preferences/wsjtx.plist",
+    "~/Library/Saved Application State/wsjtx.savedState"
+  ]
+end
