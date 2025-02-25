038e7107f801e4bd105c0f40428abf79343087f4
Stuart Henderson <stuart@craggiemore.com>
2025-02-25 19:51:49 +0000

    Update SkookumLogger to version 5.3 and change download URL to latest beta

--- Casks/skookumlogger.rb
+++ Casks/skookumlogger.rb
@@ -1,8 +1,8 @@
 cask "skookumlogger" do
-  version "5.2"
+  version "5.3"
   sha256 :no_check
 
-  url "https://www.k1gq.net/SkookumLoggerDownloads/SkookumLoggerRelease.zip"
+  url "https://www.k1gq.net/SkookumLoggerDownloads/SkookumLoggerLatestBeta.zip"
   name "SkookumLogger"
   desc "Contest logging software by K1GQ"
   homepage "https://www.k1gq.net/SkookumLogger/"
