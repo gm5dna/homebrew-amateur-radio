cddfc342c83c43618daa6d3ff3729fa2ff591414
Stuart Henderson <stuart@craggiemore.com>
2025-07-07 18:57:44 +0100

    Update HAMRS Pro to version 2.41.1 and remove SHA256 checksums for arm and intel architectures; update WaveLogGate to version 1.1.6 and remove SHA256 checksum; update WSJT-X Improved to version 2.7.1,250501 and fix download URLs.

--- Casks/wsjtx-improved.rb
+++ Casks/wsjtx-improved.rb
@@ -1,15 +1,15 @@
 cask "wsjtx-improved" do
-  version "2.7.1,250106-RC8"
+  version "2.7.1,250501"
 
   on_arm do
     sha256 :no_check
 
-    url "https://downloads.sourceforge.net/wsjt-x-improved/wsjtx-#{version.csv.first}-devel_improved_PLUS_#{version.csv.second}_M1.zip"
+    url "https://downloads.sourceforge.net/wsjt-x-improved/wsjtx-v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}_improved_PLUS_#{version.csv.second}_M1.zip"
   end
   on_intel do
     sha256 :no_check
 
-    url "https://downloads.sourceforge.net/wsjt-x-improved/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}-devel_improved_PLUS_#{version.csv.second}_Darwin.zip"
+    url "https://downloads.sourceforge.net/wsjt-x-improved/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}_improved_PLUS_#{version.csv.second}_Darwin.zip"
   end
 
   name "WSJT-X Improved"
