e9f15bda162c826d3b2ccda801b70cb0a006dfa5
Stuart Henderson <stuart@craggiemore.com>
2025-07-07 19:19:19 +0100

    Update wsjtx-improved to version 2.8.0,250501; add verified URLs and remove redundant sha256 checks

--- Casks/wsjtx-improved.rb
+++ Casks/wsjtx-improved.rb
@@ -1,15 +1,15 @@
 cask "wsjtx-improved" do
   version "2.8.0,250501"
+  sha256 :no_check
 
   on_arm do
-    sha256 :no_check
-
-    url "https://downloads.sourceforge.net/wsjt-x-improved/wsjtx-v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}_improved_PLUS_#{version.csv.second}_M1.zip"
+    url "https://downloads.sourceforge.net/wsjt-x-improved/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}_improved_PLUS_#{version.csv.second}_M1.zip",
+        verified: "sourceforge.net/projects/wsjt-x-improved/"
   end
-  on_intel do
-    sha256 :no_check
 
-    url "https://downloads.sourceforge.net/wsjt-x-improved/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}_improved_PLUS_#{version.csv.second}_Darwin.zip"
+  on_intel do
+    url "https://downloads.sourceforge.net/wsjt-x-improved/WSJT-X_v#{version.csv.first}/macOS/wsjtx-#{version.csv.first}_improved_PLUS_#{version.csv.second}_Darwin.zip",
+        verified: "sourceforge.net/projects/wsjt-x-improved/"
   end
 
   name "WSJT-X Improved"
