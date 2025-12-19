855ea6b612da00777076444b563716d0abae8d0f
Stuart Henderson <stuart@craggiemore.com>
2025-12-19 20:29:48 +0000

    Update FreeDV cask to version 2.1.0 and set sha256 checksum

--- Casks/freedv.rb
+++ Casks/freedv.rb
@@ -1,6 +1,6 @@
 cask "freedv" do
-  version "2.0.2"
-  sha256 :no_check
+  version "2.1.0"
+  sha256 "cf53592391e3ed6d5b40b546603b01dff63e74d3a25b821cf62dfd420823d30f"
 
   url "https://github.com/drowe67/freedv-gui/releases/download/v#{version}/FreeDV.dmg",
       verified: "github.com/drowe67/freedv-gui/"
