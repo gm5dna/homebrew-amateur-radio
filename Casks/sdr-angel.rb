4d49fe2b05907475abb15cbd4d92174edb135745
Stuart Henderson <stuart@craggiemore.com>
2026-05-04 12:01:52 +0100

    sdr-angel: update to 7.25.0

--- Casks/sdr-angel.rb
+++ Casks/sdr-angel.rb
@@ -1,13 +1,13 @@
 cask "sdr-angel" do
-  version "7.24.0"
+  version "7.25.0"
 
   on_arm do
-    sha256 "61e08d28ed7540c20539dbfa5a3dd7ecb7e35309ba9babc659cf58473b34c1af"
+    sha256 "47f94d3a1e97eb8437460891850959a591e2f50298bfea6835e4dbf30f52eb1c"
 
-    url "https://github.com/f4exb/sdrangel/releases/download/v#{version}/sdrangel-#{version}_mac-14.8.4_arm64.dmg"
+    url "https://github.com/f4exb/sdrangel/releases/download/v#{version}/sdrangel-#{version}_mac-14.8.5_arm64.dmg"
   end
   on_intel do
-    sha256 "31d6bc6cd7375826cdaaccc76317e43749c7f6a6ca1368bf9ce65105775d7c94"
+    sha256 "57252af26bbfee6fec21a47f4f89a59dc820922dd7a468a750ed90655be63320"
 
     url "https://github.com/f4exb/sdrangel/releases/download/v#{version}/sdrangel-#{version}_mac-15.7.5_x86_64.dmg"
   end
