0c65953702d0754de9bb858ae3ced88cbd8c2355
Stuart <stuart@craggiemore.com>
2026-07-07 03:09:50 +0000

    waveloggate: 2.0.7 → 2.0.8

--- Casks/waveloggate.rb
+++ Casks/waveloggate.rb
@@ -1,13 +1,13 @@
 cask "waveloggate" do
-  version "2.0.7"
+  version "2.0.8"
 
   on_arm do
-    sha256 "b7fabcc868ae5762ee2a14dbccee040257f19910725709ddb2de2219bd9a6b0e"
+    sha256 "a698d5e387660c4f3e17a7c4b9ff33a8836e272642ef0c4601bd1eec79cc7d36"
 
     url "https://github.com/wavelog/WaveLogGate/releases/download/v#{version}/WavelogGate-darwin-arm64.dmg"
   end
   on_intel do
-    sha256 "51a6b00508cd2d527f4df9112679a0b4b8284f338a31dab866472fef21e6d6f7"
+    sha256 "dd8448b0ac24a55ece32d4aff510827aea47c81372f840f073cf1b662286b855"
 
     url "https://github.com/wavelog/WaveLogGate/releases/download/v#{version}/WavelogGate-darwin-amd64.dmg"
   end
